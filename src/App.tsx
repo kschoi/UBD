c3087edd51012ba3ec42d81f50d481962dc90eaa
simsimjae <191429@ssg.com>
2019-12-20 13:35:46 +0900

    eslint, prettier 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,28 @@
-import React, {useEffect, useState} from 'react';
-import {GEditor, GrapesPluginType} from "grapesjs-react";
-import 'grapesjs/dist/css/grapes.min.css';
-import {timerPluginRef} from "./timer/consts";
-import addTimerPlugin from './timer';
-import TemplateDisplay from "./templateDisplay";
+import React, { useEffect, useState } from "react"
+import { GEditor, GrapesPluginType } from "grapesjs-react"
+import "grapesjs/dist/css/grapes.min.css"
+import { timerPluginRef } from "./timer/consts"
+import addTimerPlugin from "./timer"
+import TemplateDisplay from "./templateDisplay"
 
 const App: React.FC = () => {
-
-    const [htmlString, setHtmlString] = useState(null);
-    const [cssString, setCssString] = useState("");
-    const [pluginLoaded, setPluginLoaded] = useState(false);
+    const [htmlString, setHtmlString] = useState(null)
+    const [cssString, setCssString] = useState("")
+    const [pluginLoaded, setPluginLoaded] = useState(false)
 
     if (!pluginLoaded) {
         // Pass the state setters to the timer plugin, so that each time the bell is pressed these gets called
         // and the TemplateDisplay gets updated withthe new values
-        addTimerPlugin(setHtmlString, setCssString);
-        setPluginLoaded(true);
+        addTimerPlugin(setHtmlString, setCssString)
+        setPluginLoaded(true)
     }
 
     return (
         <>
-            <GEditor id="geditor" plugins={[timerPluginRef]}/>
+            <GEditor id="geditor" plugins={[timerPluginRef]} />
             <TemplateDisplay jsxString={htmlString} cssString={cssString} />
         </>
-    );
+    )
 }
 
-export default App;
+export default App
